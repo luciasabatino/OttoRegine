db9bedc07835ceb83b65442bd08d120358858cda
NAME <NAME@172.22.57.51>
2018-04-10 18:07:52 +0200

    Versione completa

--- src/it/polito/tdp/regine/Regine.java
+++ src/it/polito/tdp/regine/Regine.java
@@ -11,21 +11,56 @@ import java.util.List;
  */
 public class Regine {
 	
-	private List<Pos> regine ;
-	
-	public Regine() {
-		regine = new ArrayList<>() ;
+	public List<Integer> posiziona(int N){
+		List<Integer> soluzione = new ArrayList<Integer>();
+		if(cerca(soluzione,N,0))
+			return soluzione;
+		else
+			return null;
 	}
 	
-	public void add(Pos regina) {
-		if(regine.contains(regina)) {
-			throw new IllegalArgumentException("C'è già una regina in posizione "+regina) ;
+	/**
+	 * Assumendo che scacchiera sia piena nelle righe da 0 a livello-1,
+	 * determina le possibili posizioni per una regina alla riga==livello
+	 * e prova ricorsivamente a posizionarla
+	 * 
+	 * @param scacchiera lista delle posizioni (colonna) delle regine precedenti
+	 * @param N dimensione scacchiera
+	 * @param livello riga a cui deve lavorare la ricorsione
+	 */
+	private boolean cerca(List<Integer> scacchiera, int N, int livello) {
+		if(livello==N) {
+			//caso terminale, soluzione trovata
+			return true;
 		}
-		regine.add(regina) ;
+		for(int mossa=0; mossa<N;mossa++) {
+			//Verifico che quella casella di mossa non vada in conflitto con le regine precedenti
+			if(posizioneSicura(scacchiera,livello,N,mossa)) {
+			scacchiera.add(mossa);
+			if(cerca(scacchiera,N,livello+1))
+				return true;
+			scacchiera.remove(livello); //backtrack
+			}
+		}
+		return false;
 	}
-	
-	public void removeLast() {
-		regine.remove(regine.size()-1) ;
+
+	private boolean posizioneSicura(List<Integer> scacchiera, int livello, int n, int mossa) {
+		for(int riga=0; riga<livello; riga++) {
+			//verifica delle colonne
+			if(scacchiera.get(riga)==mossa)
+				return false;
+			
+			//verifica diagonale destra: R1-C1 ==R2-C2
+			if(riga-scacchiera.get(riga)==livello-mossa)
+				return false;
+				
+			//verifica diagonale sinistra: R1+C1 = R2+C2
+			if(riga+scacchiera.get(riga)==livello+mossa)
+				return false;
+		}
+		
+		return true;
 	}
 
 }
